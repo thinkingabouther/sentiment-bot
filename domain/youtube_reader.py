f686fef3b3d1163e69421c86638b1e1e6447cbcb
GurovaAnya <anna30850@gmail.com>
2021-12-27 01:22:09 +0500

    fix

--- domain/youtube_reader.py
+++ domain/youtube_reader.py
@@ -17,8 +17,7 @@ class YouTubeReader:
         while "nextPageToken" in data and int(params["maxResults"]) > 0:
             params["pageToken"] = data["nextPageToken"]
             data = requests.get(self.endpoint, params=params).json()
-            result.append(
-                [item['snippet']['topLevelComment']['snippet']['textDisplay'] for item in data['items']])
+            result += [item['snippet']['topLevelComment']['snippet']['textDisplay'] for item in data['items']]
             params["maxResults"] = int(params["maxResults"]) - int(data["pageInfo"]["resultsPerPage"])
 
         return result
