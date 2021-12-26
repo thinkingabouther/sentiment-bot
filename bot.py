c778cb7b1f348901ddb5e8a84a24849bf6ab295c
r.salakhov <r.salakhov@xsolla.com>
2021-12-26 23:30:01 +0500

    fix

--- bot.py
+++ bot.py
@@ -14,17 +14,12 @@ KEY = os.environ['AZURE_API_KEY']
 ENTERING_LINK, ENTERING_MAX_COMMENTS, GETTING_RESULT = range(3)
 
 def start(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text(
-        "hello"
-    )
-    return ENTERING_LINK
-
-def enter_link(update: Update, context: CallbackContext) -> int:
     """Start the conversation and ask user for input."""
     update.message.reply_text(
         "Enter link to YT video"
     )
     return ENTERING_MAX_COMMENTS
+    
 
 def enter_max_comments(update: Update, context: CallbackContext) -> int:
     """Ask the user for a max comments count."""
@@ -79,10 +74,10 @@ class Bot:
         dp.add_handler(conv_handler)
 
 
-        # self.updater.start_webhook(listen="0.0.0.0",
-        #                            port=int(port),
-        #                            url_path=token)
-        # self.updater.bot.setWebhook(app_url + token)
+        self.updater.start_webhook(listen="0.0.0.0",
+                                   port=int(port),
+                                   url_path=token)
+        self.updater.bot.setWebhook(app_url + token)
         self.updater.start_polling()
 
     def idle(self):
